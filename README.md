60d6537ced950b1c913d408672db67785ea1a2f4
deepakraj-dj <deepakrajdj23@gmail.com>
2026-06-11 19:56:14 +0530

    initial push

---
+++ README.md
@@ -0,0 +1,2 @@
+# linux-health-monitor
+Bash-based Linux monitor that tracks CPU, memory, disk, and failed SSH logins — sends real-time alerts straight to Telegram.
---
+++ healthck.sh
@@ -0,0 +1,46 @@
+
+#!/bin/bash
+
+TOKEN="${{ secrets.TELEGRAM_API_TOKEN }}"
+CHAT_ID="${{ secrets.TBOT_API_USERNAME }}"
+echo "---------------------------------------------------"
+echo "...Health Checker..."
+sleep 2
+
+RAM=$(free -h | grep "Mem" | awk '{print $4}')
+echo "Available RAM IS: $RAM"
+
+DS=$(df -h | grep "sda2" | awk '{print $4}')
+echo "Free Space on disk : $DS"
+
+CPU=$(mpstat 1 1 | grep "Average" | awk '{print 100-$12}')
+echo "CPU Usage is: $CPU%"
+
+up=$(uptime -p)
+echo "System Uptime is $up"
+
+log_file="/var/log/auth.log"
+echo "The last Failed SSH Attempts"
+sleep 3
+SSH_ATTEMPTS=$(grep -i "failed password" $log_file | tail)
+
+# NOW create the message with all data
+message="Boss, The scan has been completeted here is the report, 
+
+📊 Available RAM : $RAM
+Free Space on disk : $DS
+CPU Usage is: $CPU%
+System Uptime is $up
+The last Failed SSH Attempts
+$SSH_ATTEMPTS
+
+Thanks, let me know if i want to assist with anything 😄
+"
+
+# Send to Telegram with proper quoting
+curl -X POST https://api.telegram.org/bot${TOKEN}/sendMessage \
+  -d chat_id=${CHAT_ID} \
+  -d "text=${message}" \
+  -d parse_mode="Markdown"
+
+
